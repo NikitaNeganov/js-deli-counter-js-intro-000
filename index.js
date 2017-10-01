f438aafa841e790e7adf9f9c9ecc5d090ed4cc6a
Nikita Neganov <nikitaneganov@icloud.com>
2017-10-01 07:27:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ function takeANumber(katzDeliLine, name){
   katzDeliLine.push(name);
   return (`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`)
 }
+function nowServing(line){
+  if (line.length < 1){
+    return 'There is nobody waiting to be served!';
+  } else if (line.length >= 1){
+    return `Currently serving ${line[0]}`;
+    line.shift();
+  }
+}
