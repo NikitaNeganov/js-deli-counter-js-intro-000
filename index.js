dbe8e6932fa6a45481329faa3cc0b107709053be
Nikita Neganov <nikitaneganov@icloud.com>
2017-10-01 07:29:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ function nowServing(line){
   if (line.length < 1){
     return 'There is nobody waiting to be served!';
   } else if (line.length >= 1){
-    return `Currently serving ${line[0]}.`;
+    var message = `Currently serving ${line[0]}.`;
     line.shift();
+    return message;
   }
 }
